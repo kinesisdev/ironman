2af1b92a917ea5364723d0c8fc9d1282f8a5e228
Long To <lto@draftkings.com>
2020-04-28 06:11:11 -0400

    KIN-8: Test

--- kmc.lambda/add-product.js
+++ kmc.lambda/add-product.js
@@ -1,53 +1,53 @@
 import validate from "./libs/validations/add-product-validation";
 
-import * as dynamoDbLib from "./libs/dynamoDb-lib";
+import dynamoDbLib from "./libs/dynamoDb-lib";
 
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context, callback) {
     // Request body is passed in as a JSON encoded string in 'event.body'
-    const data = JSON.parse(event.body);
-
-    const err = validate(data);
-
-    if (err) {
-        return failure(500, err.message);
-    }
-
-    const putParams = {
-        TableName: process.env.KMC_PRODUCT,
-        Item: {
-            addedUser: event.requestContext.identity.cognitoIdentityId,
-            productId: data.productId,
-            type: data.type || "",
-            weight: data.type || "0",
-            isSold: data.isSold || false,
-            createdAt: Date.now(),
-            updatedAt: Date.now()
-        }
-    };
-
-    const getParams = {
-        TableName: process.env.KMC_PRODUCT,
-        Key: {
-            productId: data.productId
-        }
-    };
-
-    try {
-        var result = await dynamoDbLib.call("get", getParams);
-        if (result.Item) {
-            return success({message: "Account is already in database"});
-        } else {
-            await dynamoDbLib.call("put", putParams);
-            var response = {
-                message: "Successfully add account into database",
-                productId: putParams.Item.productId
-            };
-            return success(response);
-        }
-    } catch (e) {
-        return failure(500, { message: e.message });
-    }
+    // const data = JSON.parse(event.body);
+    //
+    // const err = validate(data);
+    //
+    // if (err) {
+    //     return failure(500, err.message);
+    // }
+    //
+    // const putParams = {
+    //     TableName: process.env.KMC_PRODUCT,
+    //     Item: {
+    //         addedUser: event.requestContext.identity.cognitoIdentityId,
+    //         productId: data.productId,
+    //         type: data.type || "",
+    //         weight: data.type || "0",
+    //         isSold: data.isSold || false,
+    //         createdAt: Date.now(),
+    //         updatedAt: Date.now()
+    //     }
+    // };
+    //
+    // const getParams = {
+    //     TableName: process.env.KMC_PRODUCT,
+    //     Key: {
+    //         productId: data.productId
+    //     }
+    // };
+    //
+    // try {
+    //     var result = await dynamoDbLib.call("get", getParams);
+    //     if (result.Item) {
+    //         return success({message: "Account is already in database"});
+    //     } else {
+    //         await dynamoDbLib.call("put", putParams);
+    //         var response = {
+    //             message: "Successfully add account into database",
+    //             productId: putParams.Item.productId
+    //         };
+    //         return success(response);
+    //     }
+    // } catch (e) {
+    //     return failure(500, { message: e.message });
+    // }
 
 }
